f9eac8b43fe3ff5e04c09a1160794ea921112508
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2022-08-16 17:52:38 -0500

    remove node_modules

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a633110ea16b3cf"; }
+/******/ 		__webpack_require__.h = function() { return "7b6d49907cf002d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Modalbox.js
+++ src/components/Modalbox.js
@@ -8,7 +8,7 @@ const Modalbox = ({ close, value }) => {
       tag: 'PERN Stack',
       desc: [
         {
-          p: 'User registers with phone number, allows user to be as anonymous as possible. Logged in user is able to submit a list of things needed as well as a location of where the trusted admin is to drop off the requested items. Admin is able to see a list of orders with the items requested from all users.'
+          p: 'User registers with phone number, allows user to be as anonymous as possible. Logged in user is able to submit a list of things needed as well as a location of where the trusted admin is to drop off the requested items. Admin is able to see a list of orders with the items requested from all users.',
         },
       ],
     },
@@ -26,7 +26,7 @@ const Modalbox = ({ close, value }) => {
       tag: 'JavaScript, React, Node.js, Express.js',
       desc: [
         {
-          p: 'Pet adoption site where user submits their name and waits in line for others to adopt. Once it\'s user\'s turn, they get to adopt a cat or dog. This project is meant to show my understanding of the queue data structure.'
+          p: "Pet adoption site where user submits their name and waits in line for others to adopt. Once it's user's turn, they get to adopt a cat or dog. This project is meant to show my understanding of the queue data structure.",
         },
       ],
     },
@@ -35,7 +35,7 @@ const Modalbox = ({ close, value }) => {
       tag: 'PERN Stack',
       desc: [
         {
-          p: 'This project uses spaced repetition to teach Italian. The user guesses the translation for the Italian word and the next page shows if they got the word right or wrong. For each user, score is kept on how many times they have guessed on the word, the correct and incorrect number of guesses on each word.'
+          p: 'This project uses spaced repetition to teach Italian. The user guesses the translation for the Italian word and the next page shows if they got the word right or wrong. For each user, score is kept on how many times they have guessed on the word, the correct and incorrect number of guesses on each word.',
         },
       ],
     },
@@ -47,19 +47,19 @@ const Modalbox = ({ close, value }) => {
   }, [index]);
 
   return (
-    <div className="resumo_fn_modalbox opened">
+    <div className='resumo_fn_modalbox opened'>
       <a
-        className="extra_closer"
-        href="#"
+        className='extra_closer'
+        href='#'
         onClick={(e) => {
           e.preventDefault();
           close(false);
         }}
       />
-      <div className="box_inner">
+      <div className='box_inner'>
         <a
-          className="closer"
-          href="#"
+          className='closer'
+          href='#'
           onClick={(e) => {
             e.preventDefault();
             close(false);
@@ -67,23 +67,23 @@ const Modalbox = ({ close, value }) => {
         >
           <span />
         </a>
-        <div className="modal_content">
-          <div className="modal_in">
+        <div className='modal_content'>
+          <div className='modal_in'>
             {data.map(
               (d, i) =>
                 i + 1 == index && (
                   <div key={i}>
-                    <p className="fn__cat">{d.tag}</p>
-                    <h3 className="fn__title">{d.name}</h3>
-                    <div className="img_holder">
-                      <img src="/img/thumb/square.jpg" alt="" />
+                    <p className='fn__cat'>{d.tag}</p>
+                    <h3 className='fn__title'>{d.name}</h3>
+                    <div className='img_holder'>
+                      <img src='/img/thumb/square.jpg' alt='' />
                       <div
-                        className="abs_img"
+                        className='abs_img'
                         data-bg-img={`/img/portfolio/${index}.jpg`}
                       />
                     </div>
                     {d.desc.map((des, i) => (
-                      <p key={i} className="fn__desc">
+                      <p key={i} className='fn__desc'>
                         {des.p}
                       </p>
                     ))}
@@ -92,31 +92,31 @@ const Modalbox = ({ close, value }) => {
             )}
           </div>
 
-          <div className="fn__nav" data-from="portfolio" data-index="1">
+          <div className='fn__nav' data-from='portfolio' data-index='1'>
             <a
-              href="#"
-              className="prev"
+              href='#'
+              className='prev'
               onClick={(e) => {
                 e.preventDefault();
                 setIndex(index == 1 ? 4 : index - 1);
               }}
             >
-              <span className="text">Prev</span>
-              <span className="arrow_wrapper">
-                <span className="arrow"></span>
+              <span className='text'>Prev</span>
+              <span className='arrow_wrapper'>
+                <span className='arrow'></span>
               </span>
             </a>
             <a
-              href="#"
-              className="next"
+              href='#'
+              className='next'
               onClick={(e) => {
                 e.preventDefault();
                 setIndex(index == 4 ? 1 : index + 1);
               }}
             >
-              <span className="text">Next</span>
-              <span className="arrow_wrapper">
-                <span className="arrow"></span>
+              <span className='text'>Next</span>
+              <span className='arrow_wrapper'>
+                <span className='arrow'></span>
               </span>
             </a>
           </div>
